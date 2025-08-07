805a84b39e9cfdd2731fa944c4938ae379e5f187
Nikhil Siwan <developer.nikk@gmail.com>
2025-08-07 12:18:49 +0530

    Initial commit
    Generated by react-native@0.78.2

---
+++ android/app/.cxx/Debug/2gj375e4/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/fbjni/fbjniConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET fbjni::fbjni)
+add_library(fbjni::fbjni SHARED IMPORTED)
+set_target_properties(fbjni::fbjni PROPERTIES
+    IMPORTED_LOCATION "/Users/ashishworkspace/.gradle/caches/8.12.1/transforms/ebe6f558636382523f2cf3569db5b8da/transformed/fbjni-0.7.0/prefab/modules/fbjni/libs/android.armeabi-v7a/libfbjni.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/ashishworkspace/.gradle/caches/8.12.1/transforms/ebe6f558636382523f2cf3569db5b8da/transformed/fbjni-0.7.0/prefab/modules/fbjni/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
